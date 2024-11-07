26f0666cd256e0486e486b2153ce376d587f1115
Aryan Chettri <143951956+aryan-chettri@users.noreply.github.com>
2024-11-07 16:55:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContext = "Hello World";
+
+// let myvariable = "Aryan";
+// console.log(myvariable);
+
+// let myname = "Aryan";
+
+// if (myname === "Piyush") {
+//   alert("This is aryan");
+// } else {
+//   alert("This is not aryan");
+// }
+
+
+// let thisname = document.querySelector('h1');
+
+// thisname.textContent = "My name is Hello World";
+
+// console.log(thisname);
+
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+//   });
